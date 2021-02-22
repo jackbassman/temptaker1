ef70b3fc23e8a0c9d7549d1e1cba1731a7fecca4
jackbassman <79383566+jackbassman@users.noreply.github.com>
2021-02-22 16:31:02 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-temp = 0
 tempF = 0
+temp = 0
+
+def on_button_pressed_a():
+    basic.show_number(tempF)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    basic.show_number(temp)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_logo_pressed():
+    basic.show_number(control.device_serial_number())
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
 
 def on_forever():
     global temp, tempF
     temp = input.temperature()
     tempF = temp * 2
     tempF += 30
-    basic.show_number(tempF)
 basic.forever(on_forever)
