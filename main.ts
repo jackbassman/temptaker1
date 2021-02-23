f6a7b6666306bbcb2f352bb4999abb37730caeb1
jackbassman <79383566+jackbassman@users.noreply.github.com>
2021-02-23 12:06:16 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,22 @@
-let tempF = 0
-let temp = 0
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(tempF)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(temp)
+    tempLOCAL = input.temperature()
+    tempLOCAL += tempLOCAL * 2
+    tempLOCAL += 30
+    basic.showNumber(tempLOCAL)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     basic.showNumber(control.deviceSerialNumber())
 })
+let temp = 0
+let tempLOCAL = 0
+let tempF = 0
+radio.setGroup(1)
 basic.forever(function () {
     temp = input.temperature()
     tempF = temp * 2
     tempF += 30
+    radio.sendNumber(tempF)
 })
